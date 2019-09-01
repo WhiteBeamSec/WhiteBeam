ab80b128a5b56940fc1eead9e1985b0bdbd18748
noproto <noproto@zeroday.engineering>
2019-09-01 18:24:50 -0400

    Error handling for hashing

--- src/library/common/hash.rs
+++ src/library/common/hash.rs
@@ -5,11 +5,16 @@ fn common_hash_algo() -> Sha3_256 {
     Sha3_256::new()
 }
 
-// TODO: Error handling
 pub fn common_hash_file(path: &str) -> String {
-    let mut file = fs::File::open(&path).unwrap();
+    let mut file = match fs::File::open(&path) {
+        Err(why) => return "0".repeat(64),
+        Ok(file) => file
+    };
     let mut hasher = common_hash_algo();
-    let _n = io::copy(&mut file, &mut hasher).unwrap();
+    let _n = match io::copy(&mut file, &mut hasher) {
+        Err(why) => return "0".repeat(64),
+        Ok(cnt) => cnt
+    };
     let hash = hasher.result();
     hex::encode(hash)
 }
