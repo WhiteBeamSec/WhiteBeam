52ef2704acb8d12d51d4eacd57034ec2e03ca4ad
noproto <noproto@zeroday.engineering>
2019-09-01 19:18:58 -0400

    Warn unused

--- src/library/common/hash.rs
+++ src/library/common/hash.rs
@@ -7,12 +7,12 @@ fn common_hash_algo() -> Sha3_256 {
 
 pub fn common_hash_file(path: &str) -> String {
     let mut file = match fs::File::open(&path) {
-        Err(why) => return "0".repeat(64),
+        Err(_why) => return "0".repeat(64),
         Ok(file) => file
     };
     let mut hasher = common_hash_algo();
     let _n = match io::copy(&mut file, &mut hasher) {
-        Err(why) => return "0".repeat(64),
+        Err(_why) => return "0".repeat(64),
         Ok(cnt) => cnt
     };
     let hash = hasher.result();
