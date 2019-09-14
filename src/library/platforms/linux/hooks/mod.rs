907b0fa2651278d20d0af2aa982f76055b9fe9d0
noproto <noproto@zeroday.engineering>
2019-09-14 17:46:08 -0400

    Scope file access functions

--- src/library/platforms/linux/hooks/mod.rs
+++ src/library/platforms/linux/hooks/mod.rs
@@ -1,11 +1,10 @@
 /*
-Primary hook
+exec hooks
 */
+// Primary hook
 mod execve;
 
-/*
-Secondary hooks
-*/
+// Secondary hooks
 mod execle;
 mod execvpe;
 mod fexecve;
@@ -13,3 +12,20 @@ mod execl;
 mod execlp;
 mod execv;
 mod execvp;
+
+/*
+TODO: Protect mem and other system files using mode
+O_RDWR or O_WRONLY is prohibited, including implicitly (creat)
+*/
+// mod creat
+// mod creat64
+// mod fopen
+// mod fopen64
+// mod freopen
+// mod freopen64
+// mod open
+// mod open64
+// mod openat
+// mod openat64
+// mod open_by_handle_at
+// TODO: are fd functions necessary? They depend on above.
