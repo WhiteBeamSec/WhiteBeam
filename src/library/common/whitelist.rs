ab924a48f727f56ba77b05e220e60edef36e5289
noproto <noproto@zeroday.engineering>
2019-09-16 23:50:41 -0400

    Move array out of whitelisting function

--- src/library/common/whitelist.rs
+++ src/library/common/whitelist.rs
@@ -1,4 +1,3 @@
-//use std::env;
 #[cfg(target_os = "windows")]
 use crate::library::platforms::windows as platform;
 #[cfg(target_os = "linux")]
@@ -7,32 +6,35 @@ use crate::library::platforms::linux as platform;
 use crate::library::platforms::macos as platform;
 use std::{ffi::OsString};
 
+// Hardcoded whitelist data for setup
+static ENV_BLACKLIST: &'static [&str] = &[
+    "LD_PRELOAD",
+    "LD_AUDIT",
+    "LD_LIBRARY_PATH"
+];
+
+static WHITELIST: &'static [(&str, bool, &str)] = &[
+    // Tuple of (permitted program, allow unsafe environment variables, SHA3-256 hexdigest)
+    // Shells
+    ("/bin/bash", false, "ANY"),
+    ("/bin/sh", false, "ANY"),
+    // WhiteBeam
+    ("/opt/WhiteBeam/whitebeam", false, "ANY"),
+    ("/usr/local/bin/whitebeam", false, "ANY")
+];
+
 pub fn is_whitelisted(program: &str, env: &Vec<(OsString, OsString)>, hexdigest: &str) -> bool {
     let mut unsafe_env = false;
     let mut allow_exec = false;
-    let env_blacklist = [
-            "LD_PRELOAD",
-            "LD_AUDIT",
-            "LD_LIBRARY_PATH"
-    ];
-    let whitelist = [
-        // Tuple of (permitted program, allow unsafe environment variables, SHA3-256 hexdigest)
-        // Shells
-        ("/bin/bash", false, "ANY"),
-        ("/bin/sh", false, "ANY"),
-        // WhiteBeam
-        ("/opt/WhiteBeam/whitebeam", false, "ANY"),
-        ("/usr/local/bin/whitebeam", false, "ANY")
-    ];
     // TODO: Parse cache file
     let _cache_file = platform::get_cache_file();
     for env_var in env {
-        if env_blacklist.contains(&env_var.0.to_str().unwrap()) {
+        if ENV_BLACKLIST.contains(&env_var.0.to_str().unwrap()) {
             unsafe_env = true;
             break;
         }
     }
-    for (allowed_program, allow_unsafe, allowed_hash) in whitelist.iter() {
+    for (allowed_program, allow_unsafe, allowed_hash) in WHITELIST.iter() {
         if  (&program == allowed_program) &&
             (&unsafe_env == allow_unsafe) &&
             ((&hexdigest == allowed_hash) || (hexdigest == "ANY")) {
