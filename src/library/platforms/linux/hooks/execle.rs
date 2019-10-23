f12a762fcf27fee3b39446f67c35554173503ec4
noproto <noproto@zeroday.engineering>
2019-10-23 18:14:05 -0400

    Unnecessary unsafe block

--- src/library/platforms/linux/hooks/execle.rs
+++ src/library/platforms/linux/hooks/execle.rs
@@ -24,11 +24,9 @@ pub unsafe extern "C" fn execle(path: *const c_char, mut args: ...) -> c_int {
     // Call execve
     static mut REAL: *const u8 = 0 as *const u8;
     static mut ONCE: ::std::sync::Once = ::std::sync::Once::new();
-    unsafe {
-        ONCE.call_once(|| {
-            REAL = crate::library::platforms::linux::hook::dlsym_next("execve\u{0}");
-        });
-    }
+    ONCE.call_once(|| {
+        REAL = crate::library::platforms::linux::hook::dlsym_next("execve\u{0}");
+    });
     let execve: unsafe extern "C" fn(path: *const c_char, argv: *const *const c_char, envp: *const *const c_char) -> c_int = unsafe { std::mem::transmute(REAL) };
     execve(path, argv, envp)
 }
