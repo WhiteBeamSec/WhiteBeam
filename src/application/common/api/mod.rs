cadd234d98cd855edc532f3f57e82b620b1e2d89
noproto <noproto@zeroday.engineering>
2019-10-26 19:00:06 -0400

    Add status route

--- src/application/common/api/mod.rs
+++ src/application/common/api/mod.rs
@@ -3,6 +3,11 @@ use warp::Filter;
 // Database
 mod db;
 
+// GET /status
+fn status() -> impl warp::Reply {
+    return "OK";
+}
+
 // POST /log/exec
 fn log_exec(exec: db::LogExecObject) -> impl warp::Reply {
     // Input to this function is untrusted
@@ -13,13 +18,20 @@ fn log_exec(exec: db::LogExecObject) -> impl warp::Reply {
 }
 
 pub fn serve() {
+    // GET /status
+    let status_route = warp::get2()
+        .and(warp::path("status"))
+        .and(warp::path::end())
+        .map(status);
+
     // POST /log/exec {"program":"whoami","hash":"..","uid":1000,ts:1566162863}
-    let routes = warp::post2()
+    let log_exec_route = warp::post2()
         .and(warp::path("log"))
         .and(warp::path("exec"))
         .and(warp::path::end())
         .and(warp::body::json())
         .map(log_exec);
 
+    let routes = status_route.or(log_exec_route);
     warp::serve(routes).run(([0, 0, 0, 0], 11998));
 }
