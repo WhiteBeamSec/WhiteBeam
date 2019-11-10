e454a83122b7001f7df68f7542fbf032a9709508
noproto <noproto@zeroday.engineering>
2019-11-10 16:55:04 -0500

    json initialization file

--- src/application/common/db.rs
+++ src/application/common/db.rs
@@ -11,31 +11,18 @@ pub struct LogExecObject {
     pub success: bool
 }
 
-fn db_init(conn: &Connection) {
-    let _res = conn.execute(
-        "CREATE TABLE config (
-            id INTEGER PRIMARY KEY,
-            parameter TEXT NOT NULL,
-            value TEXT NOT NULL
-         )",
-        rusqlite::NO_PARAMS,
-    );
-    let _res = conn.execute(
-        "CREATE TABLE logs (
-            id INTEGER PRIMARY KEY,
-            program TEXT NOT NULL,
-            hash TEXT NOT NULL,
-            uid UNSIGNED INTEGER NOT NULL,
-            ts INTEGER NOT NULL,
-            success BOOLEAN NOT NULL
-         )",
-        rusqlite::NO_PARAMS,
-    );
+#[derive(Deserialize)]
+pub struct ConfigEntry {
+    pub server_ip: String,
+    pub server_key: String,
+    pub server_type: String
 }
 
-// TODO: get_config
+pub fn get_config(db: &Connection, config_param: String) -> String {
+    db.query_row("SELECT config_value FROM config WHERE config_param = ?", params![config_param], |r| r.get(0)).unwrap()
+}
 
-pub fn insert_config(conn: &Connection, config_param: String, config_value: String) {
+pub fn insert_config(conn: &Connection, config_param: &str, config_value: &str) {
     let _res = conn.execute(
         "INSERT INTO config (config_param, config_value)
                   VALUES (?1, ?2)",
@@ -51,11 +38,49 @@ pub fn insert_exec(conn: &Connection, exec: LogExecObject) {
     );
 }
 
+fn db_init(conn: &Connection) {
+    let _res = conn.execute(
+        "CREATE TABLE config (
+            id INTEGER PRIMARY KEY,
+            config_param TEXT NOT NULL,
+            config_value TEXT NOT NULL
+         )",
+        rusqlite::NO_PARAMS,
+    );
+    #[cfg(target_os = "linux")]
+    let config_path: &Path = Path::new("/opt/WhiteBeam/data/init.json");
+    let init_config: bool = !config_path.exists();
+    if init_config {
+        // TODO: Validate init.json
+        let init_file = std::fs::File::open(config_path).unwrap();
+        let json: ConfigEntry = serde_json::from_reader(init_file).unwrap();
+        insert_config(conn, "server_ip", &json.server_ip);
+        insert_config(conn, "server_key", &json.server_key);
+        insert_config(conn, "server_type", &json.server_type);
+        std::fs::remove_file(config_path).unwrap();
+    } else {
+        insert_config(conn, "server_ip", "none");
+        insert_config(conn, "server_key", "none");
+        insert_config(conn, "server_type", "none");
+    }
+    let _res = conn.execute(
+        "CREATE TABLE logs (
+            id INTEGER PRIMARY KEY,
+            program TEXT NOT NULL,
+            hash TEXT NOT NULL,
+            uid UNSIGNED INTEGER NOT NULL,
+            ts INTEGER NOT NULL,
+            success BOOLEAN NOT NULL
+         )",
+        rusqlite::NO_PARAMS,
+    );
+}
+
 pub fn open() -> Connection {
     #[cfg(target_os = "linux")]
-    let dbpath: &Path = Path::new("/opt/WhiteBeam/data/database.sqlite");
-    let run_init: bool = !dbpath.exists();
-    let conn: Connection = Connection::open(dbpath).unwrap();
+    let db_path: &Path = Path::new("/opt/WhiteBeam/data/database.sqlite");
+    let run_init: bool = !db_path.exists();
+    let conn: Connection = Connection::open(db_path).unwrap();
     if run_init {
         db_init(&conn)
     }
