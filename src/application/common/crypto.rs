c98c1455e0ee08c40cb4ea7c07ee90843462102d
noproto <noproto@zeroday.engineering>
2019-12-22 15:58:22 -0500

    Crypto update

--- src/application/common/crypto.rs
+++ src/application/common/crypto.rs
@@ -12,49 +12,22 @@ use serde_json::json;
 use std::{io::prelude::*,
           io::Read,
           io::Write as IoWrite,
+          time::SystemTime,
           path::Path,
           fmt::Write as FmtWrite,
           num::ParseIntError};
 use sodiumoxide::crypto::{box_,
                           box_::curve25519xsalsa20poly1305::*};
 
-// TODO: Verify nonce hasn't been seen in the past hour if valid server message
+/*
+Keys
+*/
 
-#[derive(Serialize, Deserialize, Debug)]
-struct CryptoBox {
-    pubkey: String,
-    nonce: String,
-    ciphertext: String
-}
-
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
-}
-
-pub fn encode_hex(bytes: &[u8]) -> String {
-    let mut s = String::with_capacity(bytes.len() * 2);
-    for &b in bytes {
-        write!(&mut s, "{:02x}", b);
-    }
-    s
-}
-
-fn get_server_public_key() -> PublicKey {
-    let conn: rusqlite::Connection = db::db_open();
-    let public_key_string: String = db::get_config(&conn, String::from("server_key"));
-    let public_key_bytes: &[u8] = &decode_hex(&public_key_string).unwrap();
-    PublicKey::from_slice(public_key_bytes).unwrap()
-}
-
-fn verify_server_message() {
-    ()
-}
-
-fn decrypt_server_ciphertext() {
-    ()
+fn key_array_from_slice(bytes: &[u8]) -> [u8; SECRETKEYBYTES] {
+    let mut array = [0; SECRETKEYBYTES];
+    let bytes = &bytes[..array.len()]; // Panics if not enough data
+    array.copy_from_slice(bytes);
+    array
 }
 
 fn generate_client_private_key(save_path: &Path) {
@@ -64,14 +37,14 @@ fn generate_client_private_key(save_path: &Path) {
     key_file.write_all(private_key_bytes).unwrap();
 }
 
-fn key_array_from_slice(bytes: &[u8]) -> [u8; 32] {
-    let mut array = [0; 32];
-    let bytes = &bytes[..array.len()]; // panics if not enough data
-    array.copy_from_slice(bytes);
-    array
+fn get_server_public_key() -> PublicKey {
+    let conn: rusqlite::Connection = db::db_open();
+    let public_key_string: String = db::get_config(&conn, String::from("server_key"));
+    let public_key_bytes: &[u8] = &hex::decode(&public_key_string).unwrap();
+    PublicKey::from_slice(public_key_bytes).unwrap()
 }
 
-fn read_client_keypair() -> (PublicKey, SecretKey) {
+fn get_client_public_private_key() -> (PublicKey, SecretKey) {
     let key_file_path: &Path = &platform::get_data_file_path("client.key");
     let gen_key: bool = !key_file_path.exists();
     if gen_key {
@@ -80,17 +53,102 @@ fn read_client_keypair() -> (PublicKey, SecretKey) {
     let mut key_file = std::fs::File::open(key_file_path).unwrap();
     let mut private_key_bytes: Vec<u8> = Vec::new();
     key_file.read_to_end(&mut private_key_bytes).unwrap();
-    let private_key_array: [u8; 32] = key_array_from_slice(&private_key_bytes);
+    let private_key_array: [u8; SECRETKEYBYTES] = key_array_from_slice(&private_key_bytes);
     let private_key = SecretKey(private_key_array);
     let public_key = private_key.public_key();
     (public_key, private_key)
 }
 
-pub fn generate_sealed_ciphertext(plaintext: &[u8]) {
-    let (public_key, private_key) = box_::gen_keypair();
+/*
+Encoding
+*/
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Message {
+    expires: u32,
+    version: String,
+    action: String,
+    parameters: Vec<String>
+}
+
+fn json_encode_message(action: String, parameters: Vec<String>) -> String {
+    let expires = SystemTime::now()
+                            .duration_since(SystemTime::UNIX_EPOCH)
+                            .unwrap()
+                            .as_secs() as u32;
+    let version = env!("CARGO_PKG_VERSION").to_string();
+    let message_object = Message {
+        expires: expires,
+        version: version,
+        action: action,
+        parameters: parameters
+    };
+    serde_json::to_string(&message_object).unwrap()
+}
+
+fn json_decode_message(json: String) -> Message {
+    let message_object: Message = serde_json::from_str(&json).unwrap();
+    message_object
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CryptoBox {
+    pubkey: String,
+    nonce: String,
+    ciphertext: String
+}
+
+fn json_encode_crypto_box(pubkey: String, nonce: String, ciphertext: String) -> String {
+    let crypto_box_object = CryptoBox {
+        pubkey: pubkey,
+        nonce: nonce,
+        ciphertext: ciphertext
+    };
+    serde_json::to_string(&crypto_box_object).unwrap()
+}
+
+fn json_decode_crypto_box(json: String) -> CryptoBox {
+    let crypto_box_object: CryptoBox = serde_json::from_str(&json).unwrap();
+    crypto_box_object
+}
+
+/*
+Encryption
+*/
+
+fn generate_sealed_ciphertext(plaintext: &[u8]) -> Vec<u8> {
+    let (_public_key, private_key) = box_::gen_keypair();
     let server_public_key = get_server_public_key();
     let nonce = box_::gen_nonce();
-    let ciphertext = box_::seal(plaintext, &nonce, &server_public_key, &private_key);
+    box_::seal(plaintext, &nonce, &server_public_key, &private_key)
+}
+
+fn nonce_array_from_slice(bytes: &[u8]) -> [u8; NONCEBYTES] {
+    let mut array = [0; NONCEBYTES];
+    let bytes = &bytes[..array.len()]; // Panics if not enough data
+    array.copy_from_slice(bytes);
+    array
+}
+
+fn decrypt_server_ciphertext(ciphertext: &[u8], nonce_bytes: &[u8]) -> Vec<u8> {
+    let (client_public_key, client_private_key) = get_client_public_private_key();
+    let server_public_key = get_server_public_key();
+    let nonce_array: [u8; NONCEBYTES] = nonce_array_from_slice(nonce_bytes);
+    let nonce = Nonce(nonce_array);
+    box_::open(ciphertext, &nonce, &server_public_key, &client_private_key).unwrap()
+}
+
+fn verify_server_message() {
+    // TODO: Verify nonce hasn't been seen in the past hour if valid server message
+    // db::
+    // Then, crypto_sign_open
+    ()
+}
+
+pub fn process_server_message(request: String) {
+    // JSON decode
+    // Verify server message
+    ()
 }
 
 // TODO: For uploading logs
