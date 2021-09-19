1e87668ce30bb7a4d57b668ec0d61eff7e42ae31
Nathan Nye <nnye@whitebeamsec.com>
2021-09-19 19:56:40 -0400

    Correct number of arguments in ModifyInt action

--- src/library/common/action/actions/modify_int.rs
+++ src/library/common/action/actions/modify_int.rs
@@ -1,5 +1,5 @@
 build_action! { ModifyInt (_src_prog, hook, _arg_id, args, act_args, do_return, return_value) {
-    assert!(act_args.len() == 1);
+    assert!(act_args.len() == 2);
     let arg_index = act_args[0].parse::<usize>().expect("WhiteBeam: Unexpected null reference");
     let act_arg_int = act_args[1].parse::<i64>().expect("WhiteBeam: Unexpected null reference");
     args[arg_index].real = act_arg_int as usize;
