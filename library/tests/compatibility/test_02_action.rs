c5f7cf2c7bd1b8b1c94a0e46c392b4079053cbeb
Nathan Nye <nnye@whitebeamsec.com>
2022-03-27 10:43:05 -0400

    Add action test boilerplate

---
+++ library/tests/compatibility/test_02_action.rs
@@ -0,0 +1,23 @@
+/*
+TODO: Tests for:
+    AddEnvironment
+    AddInt
+    CanonicalizePath
+    CombineDirectory
+    ConsumeVariadic
+    FilterEnvironment
+    ModifyInt
+    ModifyString
+    OpenFileDescriptor
+    PrintArguments
+    RedirectFunction
+    SplitFilePath
+    VerifyCanExecute
+    VerifyCanTerminate
+    VerifyCanWrite
+    VerifyFileHash
+*/
+
+whitebeam_test!("linux", action_00_add_environment {
+    println!("Hello AddEnvironment action test for Linux!");
+});
