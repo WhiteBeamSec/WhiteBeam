bff84f2dbb1d37905c2ab761a5775e8c53d4971b
Nathan Nye <nnye@whitebeamsec.com>
2022-03-27 19:56:05 -0400

    More boilerplate for action tests

--- library/tests/compatibility/test_02_action.rs
+++ library/tests/compatibility/test_02_action.rs
@@ -1,23 +1,63 @@
-/*
-TODO: Tests for:
-    AddEnvironment
-    AddInt
-    CanonicalizePath
-    CombineDirectory
-    ConsumeVariadic
-    FilterEnvironment
-    ModifyInt
-    ModifyString
-    OpenFileDescriptor
-    PrintArguments
-    RedirectFunction
-    SplitFilePath
-    VerifyCanExecute
-    VerifyCanTerminate
-    VerifyCanWrite
-    VerifyFileHash
-*/
-
 whitebeam_test!("linux", action_00_add_environment {
     println!("Hello AddEnvironment action test for Linux!");
+});
+
+whitebeam_test!("linux", action_01_add_int {
+    println!("Hello AddInt action test for Linux!");
+});
+
+whitebeam_test!("linux", action_02_canonicalize_path {
+    println!("Hello CanonicalizePath action test for Linux!");
+});
+
+whitebeam_test!("linux", action_03_combine_directory {
+    println!("Hello CombineDirectory action test for Linux!");
+});
+
+whitebeam_test!("linux", action_04_consume_variadic {
+    println!("Hello ConsumeVariadic action test for Linux!");
+});
+
+whitebeam_test!("linux", action_05_filter_environment {
+    println!("Hello FilterEnvironment action test for Linux!");
+});
+
+whitebeam_test!("linux", action_06_modify_int {
+    println!("Hello ModifyInt action test for Linux!");
+});
+
+whitebeam_test!("linux", action_07_modify_string {
+    println!("Hello ModifyString action test for Linux!");
+});
+
+whitebeam_test!("linux", action_08_open_file_descriptor {
+    println!("Hello OpenFileDescriptor action test for Linux!");
+});
+
+whitebeam_test!("linux", action_09_print_arguments {
+    println!("Hello PrintArguments action test for Linux!");
+});
+
+whitebeam_test!("linux", action_10_redirect_function {
+    println!("Hello RedirectFunction action test for Linux!");
+});
+
+whitebeam_test!("linux", action_11_split_file_path {
+    println!("Hello SplitFilePath action test for Linux!");
+});
+
+whitebeam_test!("linux", action_12_verify_can_execute {
+    println!("Hello VerifyCanExecute action test for Linux!");
+});
+
+whitebeam_test!("linux", action_13_verify_can_terminate {
+    println!("Hello VerifyCanTerminate action test for Linux!");
+});
+
+whitebeam_test!("linux", action_14_verify_can_write {
+    println!("Hello VerifyCanWrite action test for Linux!");
+});
+
+whitebeam_test!("linux", action_15_verify_file_hash {
+    println!("Hello VerifyFileHash action test for Linux!");
 });
