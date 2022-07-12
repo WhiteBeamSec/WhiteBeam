e292932260a696498cfd9bc78f326dbdcb64011a
Nathan Nye <nnye@whitebeamsec.com>
2022-07-12 00:24:32 -0400

    Add standard execve test

--- library/tests/compatibility/test_03_hook_execution.rs
+++ library/tests/compatibility/test_03_hook_execution.rs
@@ -1,4 +1,17 @@
-// TODO: execve test that runs a common Linux utility
 whitebeam_test!("linux", execution_00_execve_standard {
-    println!("Hello standard execve compatibility test for Linux!");
+    let pid = unsafe { libc::fork() };
+    if pid == 0 {
+        unsafe { libc::execve("/usr/bin/touch\0".as_ptr() as *const libc::c_char,
+                              ["/usr/bin/touch\0".as_ptr() as *const libc::c_char, "/tmp/execve_test\0".as_ptr() as *const libc::c_char, std::ptr::null()].as_ptr(),
+                              std::ptr::null()); }
+    } else {
+        let mut status = 0;
+        unsafe { libc::waitpid(pid, &mut status, 0); }
+        assert!(status == 0);
+        let test_path = std::path::Path::new("/tmp/execve_test");
+        assert!(test_path.exists());
+        std::fs::remove_file(test_path).expect(&format!("WhiteBeam: Failed to remove {:?}", test_path));
+    }
 });
+
+// TODO: Add more Execution tests, clearly demonstrate libwhitebeam.so is loaded in child using libc::execve
