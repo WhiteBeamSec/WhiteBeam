0d5f4b8aec7b72515249f74cf19450a8e4f1d36f
Nathan Nye <nnye@whitebeamsec.com>
2022-07-13 20:54:19 -0400

    More library tests for Execution hooks

--- library/tests/compatibility/test_03_hook_execution.rs
+++ library/tests/compatibility/test_03_hook_execution.rs
@@ -1,4 +1,4 @@
-whitebeam_test!("linux", execution_00_execve_standard {
+whitebeam_test!("linux", execution_00_execve_simple {
     let pid = unsafe { libc::fork() };
     if pid == 0 {
         unsafe { libc::execve("/usr/bin/touch\0".as_ptr() as *const libc::c_char,
@@ -14,4 +14,43 @@ whitebeam_test!("linux", execution_00_execve_standard {
     }
 });
 
-// TODO: Add more Execution tests, clearly demonstrate libwhitebeam.so is loaded in child using libc::execve
+whitebeam_test!("linux", execution_01_execve_library_loaded {
+    let pid = unsafe { libc::fork() };
+    if pid == 0 {
+        unsafe { libc::execve("/usr/bin/grep\0".as_ptr() as *const libc::c_char,
+                              ["/usr/bin/grep\0".as_ptr() as *const libc::c_char, "libwhitebeam.so\0".as_ptr() as *const libc::c_char, "/proc/self/maps\0".as_ptr() as *const libc::c_char, std::ptr::null()].as_ptr(),
+                              std::ptr::null()); }
+    } else {
+        let mut status = 0;
+        unsafe { libc::waitpid(pid, &mut status, 0); }
+        assert!(status == 0);
+    }
+});
+
+whitebeam_test!("linux", execution_02_execl_simple {
+    let pid = unsafe { libc::fork() };
+    if pid == 0 {
+        unsafe { libc::execl("/usr/bin/touch\0".as_ptr() as *const libc::c_char,
+                             "/usr/bin/touch\0".as_ptr() as *const libc::c_char, "/tmp/execl_test\0".as_ptr() as *const libc::c_char, std::ptr::null() as *const libc::c_char); }
+    } else {
+        let mut status = 0;
+        unsafe { libc::waitpid(pid, &mut status, 0); }
+        assert!(status == 0);
+        let test_path = std::path::Path::new("/tmp/execl_test");
+        assert!(test_path.exists());
+        std::fs::remove_file(test_path).expect(&format!("WhiteBeam: Failed to remove {:?}", test_path));
+    }
+});
+
+// Tests for:
+// execle
+// execlp
+// execv
+// execvp
+// execvpe
+// fexecve
+// posix_spawn
+// posix_spawnp
+// dlopen
+// dlmopen
+// kill
