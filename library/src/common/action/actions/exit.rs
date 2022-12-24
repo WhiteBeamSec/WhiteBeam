a32caca2b9b084ab0a6af978889e907bf9488ab8
Nathan Nye <nnye@whitebeamsec.com>
2022-12-23 21:48:50 -0500

    Add Exit action

---
+++ library/src/common/action/actions/exit.rs
@@ -0,0 +1,8 @@
+build_action! { Exit (_par_prog, _src_prog, hook, _arg_position, args, act_args, do_return, return_value) {
+        assert!(act_args.len() == 1);
+        let status = match act_args[0].parse::<i32>() {
+            Ok(code) => code,
+            Err(_) => 0
+        };
+        std::process::exit(status);
+}}
