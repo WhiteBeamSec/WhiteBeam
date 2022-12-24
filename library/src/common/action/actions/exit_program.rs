4d4eeab53170f6c889e37a7c7574c61ab0ec04ae
Nathan Nye <nnye@whitebeamsec.com>
2022-12-24 01:38:48 -0500

    Rename Exit to ExitProgram

--- library/src/common/action/actions/exit_program.rs
+++ library/src/common/action/actions/exit_program.rs
@@ -1,4 +1,4 @@
-build_action! { Exit (_par_prog, _src_prog, hook, _arg_position, args, act_args, do_return, return_value) {
+build_action! { ExitProgram (_par_prog, _src_prog, hook, _arg_position, args, act_args, do_return, return_value) {
         assert!(act_args.len() == 1);
         let status = match act_args[0].parse::<i32>() {
             Ok(code) => code,
